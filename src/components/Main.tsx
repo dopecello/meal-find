d5890d5857da2cddc76ae3e049b3aa7696280a82
dopecello <williamlocke.cello@gmail.com>
2023-04-12 15:22:13 -0400

    done with styling and ready to test API

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,18 +6,21 @@ import Logo from './Logo'
 
 const Main = () => {
     return (
-        <div className='relative h-screen'>
+        <div>
             <div>
-                <div className='px-3 py-6 flex flex-col md:flex-row justify-between items-center text-center md:text-left'>
+                <div className='py-6 flex flex-col md:flex-row justify-between items-center text-center md:text-left'>
                     <div>
-                        <h1 className='text-[#183618] text-8xl font-bold lg:max-w-[500px] w-full pl-5'>
+                        <h1 className='text-[#183618] text-7xl font-bold lg:max-w-[500px] w-full'>
                             Find your next meal.
                         </h1>
                     </div>
                     <Logo />
                 </div>
+                <div className='flex justify-center items-center rounded-lg p-2 border border-black max-w-[200px] mx-auto cursor-pointer'>
+                    <p>Test out the API</p>
+                </div>
             </div>
-            <div className='absolute lg:static md:bottom-0'>
+            <div>
                 <Image
                     src={foodPadding}
                     alt='A roasted chicken dish mixed with tomatoes, limes, and parsley in a recyclable bowl'
