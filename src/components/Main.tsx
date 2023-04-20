36d6fe9618df6042376b0e6b5ee808dd8ff37877
dopecello <williamlocke.cello@gmail.com>
2023-04-20 16:23:33 -0400

    hard-coded card div and adjusted overall responsiveness of site

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -37,7 +37,7 @@ const Main = () => {
 
     const onSelectedValuesChanged = useCallback((values: SelectedVals) => {
         setSelectedValues(values);
-      }, []);
+    }, []);
 
     //TEST API CALL
     const getSomething = async () => {
@@ -47,7 +47,8 @@ const Main = () => {
         if (selectedValues.query) {
             apiEndpoint += `&query=${selectedValues.query}`
         } else {
-            apiEndpoint += '&query='}
+            apiEndpoint += '&query='
+        }
         if (selectedValues.maxReadyTime) {
             apiEndpoint += `&maxReadyTime=${selectedValues.maxReadyTime}`
         }
@@ -66,7 +67,7 @@ const Main = () => {
         if (selectedValues.mealTypes.length > 0) {
             apiEndpoint += `&type=${selectedValues.mealTypes.join(',')}`
         }
-       
+
         console.log(apiEndpoint)
 
         try {
@@ -77,34 +78,125 @@ const Main = () => {
             console.log(error);
         }
     }
-    
+
 
     return (
         <div>
-            <div>
-                <div className='py-6 px-8 flex flex-col md:flex-row justify-around items-center text-center md:text-left'>
-                    <div>
-                        <h1 className='text-dark-green text-7xl font-bold lg:max-w-[500px] w-full'>
-                            Find your next meal.
-                        </h1>
+            <div className='flex flex-col min-h-screen overflow-auto' id='home-screen'>
+                <div className='flex-1 z-10'>
+                    <div className='py-6 px-8 flex flex-col md:flex-row justify-around items-center text-center md:text-left'>
+                        <div>
+                            <h1 className='text-dark-green text-7xl font-bold lg:max-w-[500px] w-full'>
+                                Find your next meal.
+                            </h1>
+                        </div>
+                        <Logo />
                     </div>
-                    <Logo />
+                    <div className='flex justify-center items-center rounded-lg p-2 border-4 border-black max-w-[200px] mx-auto cursor-pointer bg-dark-green' onClick={handleFoodSearch}>
+                        <p className='font-semibold text-white'>Search Recipes</p>
+                    </div>
                 </div>
-                <div className='flex justify-center items-center rounded-lg p-2 border-4 border-black max-w-[200px] mx-auto cursor-pointer bg-dark-green' onClick={handleFoodSearch}>
-                    <p className='font-semibold text-white'>Search Recipes</p>
+                <div className='relative'>
+                    <Image
+                        src={foodPadding}
+                        alt='A roasted chicken dish mixed with tomatoes, limes, and parsley in a recyclable bowl'
+                        className='hidden md:block bg-lite-green w-full'
+                    />
+                    <Image
+                        src={foodMobile}
+                        alt='A roasted chicken dish mixed with tomatoes, limes, and parsley in a recyclable bowl'
+                        className='md:hidden bg-lite-green w-full'
+                    />
                 </div>
             </div>
-            <div>
-                <Image
-                    src={foodPadding}
-                    alt='A roasted chicken dish mixed with tomatoes, limes, and parsley in a recyclable bowl'
-                    className='-z-10 hidden md:block bg-lite-green'
-                />
-                <Image
-                    src={foodMobile}
-                    alt='A roasted chicken dish mixed with tomatoes, limes, and parsley in a recyclable bowl'
-                    className='-z-10 md:hidden bg-lite-green'
-                />
+
+            {/* Cards */}
+            <div className='w-full bg-dark-green flex flex-col items-center justify-center overflow-auto pb-4'>
+                <div className='w-full max-w-7xl'>
+                    <h3 className='text-center text-white mb-8'>Your Results</h3>
+                    <div className='w-full grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                        {/* Card component */}
+                        <div className='bg-white shadow-md rounded-lg p-4 flex flex-col mx-4'>
+                            <h1 className='text-xl font-semibold mb-2'>Recipe 1</h1>
+                            <Image src={'/'} className='w-full h-40 object-cover rounded-md mb-4' height={160} width={160} alt='placeholder' />
+                            <p className='text-gray-700 text-sm flex-grow mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex libero vitae reiciendis suscipit nobis odit et sequi! Laborum in minus sint sequi cumque, deserunt deleniti excepturi illum inventore, expedita animi.</p>
+                            <div className='flex justify-between'>
+                                <div className='text-indigo-600 font-semibold hover:text-indigo-800 cursor-pointer'>
+                                    <p>View Recipe</p>
+                                </div>
+                                <div className='text-indigo-600 font-semibold hover:text-indigo-800 cursor-pointer'>
+                                    <p>Search Similar</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* Add more cards here */}
+                        <div className='bg-white shadow-md rounded-lg p-4 flex flex-col mx-4'>
+                            <h1 className='text-xl font-semibold mb-2'>Recipe 1</h1>
+                            <Image src={'/'} className='w-full h-40 object-cover rounded-md mb-4' height={160} width={160} alt='placeholder' />
+                            <p className='text-gray-700 text-sm flex-grow mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex libero vitae reiciendis suscipit nobis odit et sequi! Laborum in minus sint sequi cumque, deserunt deleniti excepturi illum inventore, expedita animi.</p>
+                            <div className='flex justify-between'>
+                                <div className='text-indigo-600 font-semibold hover:text-indigo-800 cursor-pointer'>
+                                    <p>View Recipe</p>
+                                </div>
+                                <div className='text-indigo-600 font-semibold hover:text-indigo-800 cursor-pointer'>
+                                    <p>Search Similar</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='bg-white shadow-md rounded-lg p-4 flex flex-col mx-4'>
+                            <h1 className='text-xl font-semibold mb-2'>Recipe 1</h1>
+                            <Image src={'/'} className='w-full h-40 object-cover rounded-md mb-4' height={160} width={160} alt='placeholder' />
+                            <p className='text-gray-700 text-sm flex-grow mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex libero vitae reiciendis suscipit nobis odit et sequi! Laborum in minus sint sequi cumque, deserunt deleniti excepturi illum inventore, expedita animi.</p>
+                            <div className='flex justify-between'>
+                                <div className='text-indigo-600 font-semibold hover:text-indigo-800 cursor-pointer'>
+                                    <p>View Recipe</p>
+                                </div>
+                                <div className='text-indigo-600 font-semibold hover:text-indigo-800 cursor-pointer'>
+                                    <p>Search Similar</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='bg-white shadow-md rounded-lg p-4 flex flex-col mx-4'>
+                            <h1 className='text-xl font-semibold mb-2'>Recipe 1</h1>
+                            <Image src={'/'} className='w-full h-40 object-cover rounded-md mb-4' height={160} width={160} alt='placeholder' />
+                            <p className='text-gray-700 text-sm flex-grow mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex libero vitae reiciendis suscipit nobis odit et sequi! Laborum in minus sint sequi cumque, deserunt deleniti excepturi illum inventore, expedita animi.</p>
+                            <div className='flex justify-between'>
+                                <div className='text-indigo-600 font-semibold hover:text-indigo-800 cursor-pointer'>
+                                    <p>View Recipe</p>
+                                </div>
+                                <div className='text-indigo-600 font-semibold hover:text-indigo-800 cursor-pointer'>
+                                    <p>Search Similar</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='bg-white shadow-md rounded-lg p-4 flex flex-col mx-4'>
+                            <h1 className='text-xl font-semibold mb-2'>Recipe 1</h1>
+                            <Image src={'/'} className='w-full h-40 object-cover rounded-md mb-4' height={160} width={160} alt='placeholder' />
+                            <p className='text-gray-700 text-sm flex-grow mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex libero vitae reiciendis suscipit nobis odit et sequi! Laborum in minus sint sequi cumque, deserunt deleniti excepturi illum inventore, expedita animi.</p>
+                            <div className='flex justify-between'>
+                                <div className='text-indigo-600 font-semibold hover:text-indigo-800 cursor-pointer'>
+                                    <p>View Recipe</p>
+                                </div>
+                                <div className='text-indigo-600 font-semibold hover:text-indigo-800 cursor-pointer'>
+                                    <p>Search Similar</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='bg-white shadow-md rounded-lg p-4 flex flex-col mx-4'>
+                            <h1 className='text-xl font-semibold mb-2'>Recipe 1</h1>
+                            <Image src={'/'} className='w-full h-40 object-cover rounded-md mb-4' height={160} width={160} alt='placeholder' />
+                            <p className='text-gray-700 text-sm flex-grow mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex libero vitae reiciendis suscipit nobis odit et sequi! Laborum in minus sint sequi cumque, deserunt deleniti excepturi illum inventore, expedita animi.</p>
+                            <div className='flex justify-between'>
+                                <div className='text-indigo-600 font-semibold hover:text-indigo-800 cursor-pointer'>
+                                    <p>View Recipe</p>
+                                </div>
+                                <div className='text-indigo-600 font-semibold hover:text-indigo-800 cursor-pointer'>
+                                    <p>Search Similar</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             {/* modal */}
