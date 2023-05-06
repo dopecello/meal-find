9357a87f701a889e44d6433791a76981697501c0
dopecello <williamlocke.cello@gmail.com>
2023-05-06 12:55:12 -0400

    connection between db and prisma client!

---
+++ script_prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      username: 'Robert',
+      email: 'robertlocke.dev@gmail.com',
+      password: 'password',
+    },
+  });
+  console.log(user);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
